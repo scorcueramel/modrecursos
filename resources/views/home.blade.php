91c83e6ed6fe7c4933c33ecc5b20277ba0be7b8d
Gerardo Martinez Escate <gmartinez@loma.mss.gob.pe>
2022-08-18 21:26:22 -0500

    Seeders Cargados

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
         <div class="section-header">
             <h3 class="page__heading">Visualización General</h3>
         </div>
+        
         <div class="section-body">
             <div class="row">
                 <div class="col-lg-12">
