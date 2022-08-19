3152b2d107975fbf12f97a32d90f0d442d68acab
scorcueramel <scorcueramel@gmail.com>
2022-08-18 22:27:48 -0500

    vista general

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h3 class="page__heading">Visualización General</h3>
+            <h3 class="page__heading">Vista General</h3>
         </div>
-        
+
         <div class="section-body">
             <div class="row">
                 <div class="col-lg-12">
@@ -21,7 +21,7 @@
                                     <option value="{{$dm->id}}">{{$dm->Tipo_Concepto}}</option>
                                     @endforeach
                                 </select>
-                            </div>  
+                            </div>
                             <div class="form-group col-md">
                                 <label for="inputGroupSelect02">Conceptos Licencias</label>
                                 <select class="custom-select" id="inputGroupSelect02">
