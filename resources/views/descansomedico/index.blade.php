2a152826e64a8c28b7bccac2e2bb323b3e9b9322
scorcueramel <scorcueramel@gmail.com>
2022-09-06 22:34:13 -0500

    ...

--- resources/views/descansomedico/index.blade.php
+++ resources/views/descansomedico/index.blade.php
@@ -20,7 +20,6 @@
                                     <th style="color: #fff">REG. LAB.</th>
                                     <th style="color: #fff">UNI. ORG</th>
                                     <th style="color: #fff">I. LABORES</th>
-                                    <th style="color: #fff">C. LABORES</th>
                                     <th style="color: #fff">ESTADO</th>
                                     <th style="color: #fff">OPCIONES</th>
                                 </thead>
@@ -60,7 +59,6 @@
                     {data:'reglab_persona'},
                     {data:'uniorg_persona'},
                     {data:'fecha_inicio_persona'},
-                    {data:'fecha_cese_persona'},
                     {data:'estado_persona'},
                     {data:'detalles'}
                 ]
