72b14a40d25735a4c0b7f0db6b0d988c2d35f70d
Gerardo Martinez Escate <gmartinez@loma.mss.gob.pe>
2022-09-08 16:16:12 -0500

    metodo delete

--- routes/web.php
+++ routes/web.php
@@ -63,4 +63,6 @@ Route::group(['middleware'=>['auth']],function () {
     //TEST
     Route::get('detalle/{cod}/crear',[TestController::class, 'edit'])->name('registro.edit');
     Route::post('store', [TestController::class, 'store'])->name('store');
+    Route::post('delete/{id}', [TestController::class, 'desactivar'])->name('registro.delete');
+
 });
