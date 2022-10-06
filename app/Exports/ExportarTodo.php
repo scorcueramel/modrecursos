8314d89bc6b7f4c57d492973c2bedf8057d42b8b
scorcueramel <scorcueramel@gmail.com>
2022-10-06 12:26:43 -0500

    ...

--- app/Exports/ExportarTodo.php
+++ app/Exports/ExportarTodo.php
@@ -16,7 +16,7 @@ class ExportarTodo implements FromCollection, WithHeadings
         $registers = DB::table('registros')
         ->join('conceptos','registros.concepto_id', '=', 'conceptos.id')
         ->where('registros.estado','=',1)
-        ->get(['registros.codigo_persona','registros.tipo_documento_persona','registros.documento_persona','registros.nombre_persona','registros.reglab_persona','registros.uniorg_persona','registros.fecha_inicio_persona','registros.estado_persona','conceptos.descripcion','registros.fecha_inicio','registros.fecha_fin','registros.documento','registros.anio_periodo','registros.comentario']);
+        ->get(['registros.codigo_persona','registros.tipo_documento_persona','registros.documento_persona','registros.nombre_persona','registros.reglab_persona','registros.uniorg_persona','registros.fecha_inicio_persona','registros.estado_persona','conceptos.descripcion','registros.fecha_inicio','registros.fecha_fin','registros.documento','registros.anio_periodo','registros.comentario','registros.usuario_creador']);
 
         return $registers;
     }
@@ -24,7 +24,7 @@ class ExportarTodo implements FromCollection, WithHeadings
     public function headings(): array
     {
      return[
-        "Codigo","Tipo Documento","Nro. Documento","Nombres y Apellidos","Reg. Laboral","Und. Organica","Inicio Labores","Estado","Concepto","Inicio Permiso","Fin Permiso","Documento","Periodo","Observaciones"
+        "Codigo","Tipo Documento","Nro. Documento","Nombres y Apellidos","Reg. Laboral","Und. Organica","Inicio Labores","Estado","Concepto","Inicio Permiso","Fin Permiso","Documento","Periodo","Observaciones","Usuario"
      ];
     }
 }
